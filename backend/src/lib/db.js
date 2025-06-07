c127944c63c9bffa8bd3d9a21d8012abbd69e39a
Raj Dutta <duttabipul927@gmail.com>
2025-06-07 10:32:08 +0530

    first commit

---
+++ backend/src/lib/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const connectDB = async () => {
+    try {
+        const uri = process.env.MONGODB_URI.endsWith('/') 
+            ? `${process.env.MONGODB_URI}${DB_NAME}` 
+            : `${process.env.MONGODB_URI}/${DB_NAME}`;
+        const connectionInstance = await mongoose.connect(uri);
+
+        console.log(` \n MongoDB connected: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("Mongodb connection error", error);
+        process.exit(1);
+    }
+}
+
